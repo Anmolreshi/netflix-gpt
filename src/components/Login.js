4163644b1e7bdce2fdda5be2f1b2fe0d4bc3832c
Anmolreshi <anmolreshi@gmail.com>
2024-10-03 22:32:32 +0530

    added tailwing and componets

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <img src="https://www.netflix.com/" alt="logo"/>
+    </div>
+  )
+}
+
+export default Login;
